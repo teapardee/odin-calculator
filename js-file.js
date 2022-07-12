5d5c919938289b18308d08429ea7d3498c1ce8f3
Tyler Pardee <tjpardee@gmail.com>
2022-07-11 20:31:47 -0400

    Update CSS & Organize Files

--- js-file.js
+++ js-file.js
@@ -1,10 +1,15 @@
-const array = [7, 8, 9, 4, 5, 6, 1, 2, 3];
+const array = [7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "*", "C", 0, "=", "/"];
 
-for (i = 0; i < 15; i++) {
+for (i = 0; i < 16; i++) {
   var buttonContainer = document.querySelector(".main-container");
   var button = document.createElement("button");
   button.innerHTML = array[i];
-  button.className = ".btn";
 
+  console.log(array[i]);
+  if (Number.isInteger(array[i])) {
+    button.className = "btn-number";
+  } else {
+    button.className = "btn-function";
+  }
   buttonContainer.appendChild(button);
 }
